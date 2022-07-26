29c1e2da6f5cae3a827b9b5a83ef022026adb5a2
stecarraa <stefano@caratozzolo.me>
2022-07-26 18:18:46 +0200

    feat: upper Header

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -4,13 +4,21 @@
         <h1>
             My Header
         </h1>
+
+    <UpperHeader/>
+
     </header>
   </div>
 </template>
 
 <script>
+import UpperHeader from "./partials/UpperHeader.vue" 
+
 export default {
   name: "MyHeader",
+  components:{
+    UpperHeader,
+  }
 };
 </script>
 
---
+++ src/components/partials/UpperHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 d-flex align-items-center">
+          <div class="col-3 d d-flex align-items-center">
+            <img src="../../assets/images/logo-symbol.png" alt="" />
+            <div>Avada Pet Supplies</div>
+          </div>
+          <div class="col-3">
+            <div class="input-group">
+        
+              <div class="form-outline">
+                <input type="search" placeholder="Search.." class="form-control rounded-pill" />
+              </div>
+            </div>
+          </div>
+          <div class="col-3"><span class="fw-bold">Questions?</span> Contact us : 1.800.123.4567</div>
+          <div class="col-3 text-center">
+            <i class="fa-brands fa-shopify m-3"></i>
+            <i class="fa-regular fa-user"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UpperHeader",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/Style-scss/variables.scss";
+
+img {
+  height: 40px;
+}
+</style>
